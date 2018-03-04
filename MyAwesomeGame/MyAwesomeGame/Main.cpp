61aab50c2779394760be06b6648c2e0352e03567
Jaume <jaume.montagut.i.guix@gmail.com>
2018-03-04 11:42:35 +0100

    Fifth version of movement (works!)

--- MyAwesomeGame/MyAwesomeGame/Main.cpp
+++ MyAwesomeGame/MyAwesomeGame/Main.cpp
@@ -1,6 +1,7 @@
 #include "../SDL/include/SDL.h"
 #include <iostream>
 #include <time.h>
+#include "Header.h"
 
 #pragma comment (lib,"../SDL/libx86/SDL2.lib")
 #pragma comment (lib,"../SDL/libx86/SDL2main.lib")
@@ -12,51 +13,41 @@
 
 int main (int argc, char* argv[])
 {
+	//Declaration
 	SDL_Window * window;
 	SDL_Renderer* renderer;
 	SDL_Rect rect;
 	SDL_Rect projectile;
 	SDL_Event event;
 	bool playing;
-	bool hasMoved;
-	int randNum;
-	const Uint8 * keystate;
-	bool pressingKey[1000] = { false };
+	bool pressingW = false;
+	bool pressingA = false;
+	bool pressingS = false;
+	bool pressingD = false;
 
-
+	//Inicialization
 	SDL_Init(SDL_INIT_VIDEO);
-
 	window = SDL_CreateWindow("MyAwesomeGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENWITDH, SCREENHEIGHT, SDL_WINDOW_OPENGL);
 	if(window == NULL)
 	{
 		std::cout<<"Window not created"<<SDL_GetError<<std::endl;
 		return 1;
 	}
-
 	renderer = SDL_CreateRenderer(window, -1, 0);
-	
 	rect.x = 0;
 	rect.y = 0;
 	rect.w = RECTWITDH;
 	rect.h = RECTHEIGHT;
-	
-	projectile.x = 0;
-	projectile.y = 0;
+	projectile.x = SCREENWITDH;
+	projectile.y = SCREENHEIGHT;
 	projectile.w = 10;
 	projectile.h = 5;
-
 	playing = true;
-
 	srand(time(NULL));
-
 
 	while(playing == true)
 	{
-		//1- SDL_PollEvent();
-		//2- KeyboardEvent
-		//3- Key_Up...
-
-		//Commands
+		//Input
 		while (SDL_PollEvent(&event) != 0)
 		{
 			if(event.type == SDL_QUIT)
@@ -65,44 +56,78 @@ int main (int argc, char* argv[])
 			}
 			if(event.type == SDL_KEYDOWN)
 			{
-				pressingKey[event.key.keysym.sym] = true;
+				if (event.key.keysym.sym == SDLK_w)
+				{
+					pressingW = true;
+				}
+				if (event.key.keysym.sym == SDLK_a)
+				{
+					pressingA = true;
+				}
+				if (event.key.keysym.sym == SDLK_s)
+				{
+					pressingS = true;
+				}
+				if (event.key.keysym.sym == SDLK_d)
+				{
+					pressingD = true;
+				}
+				if(event.key.keysym.sym == SDLK_SPACE)
+				{
+					projectile.x = rect.x + RECTWITDH / 2;
+					projectile.y = rect.y + RECTHEIGHT / 2;
+				}
 			}
 			if (event.type == SDL_KEYUP)
 			{
-				pressingKey[event.key.keysym.sym] = false;
+				if(event.key.keysym.sym == SDLK_w)
+				{
+					pressingW = false;
+				}
+				if (event.key.keysym.sym == SDLK_a)
+				{
+					pressingA = false;
+				}
+				if (event.key.keysym.sym == SDLK_s)
+				{
+					pressingS = false;
+				}
+				if (event.key.keysym.sym == SDLK_d)
+				{
+					pressingD = false;
+				}
 			}
 		}
 
-		if (pressingKey[SDLK_UP] == true)
+		//Movement
+		if (pressingW == true && rect.y > 0)
 		{
 			rect.y--;
 		}
-		if (pressingKey[SDLK_LEFT] == true)
+		if (pressingA == true && rect.x > 0)
 		{
 			rect.x--;
 		}
-		if (pressingKey[SDLK_DOWN] == true)
+		if (pressingS == true && rect.y < SCREENHEIGHT - RECTHEIGHT)
 		{
 			rect.y++;
 		}
-		if (pressingKey[SDLK_RIGHT] == true)
+		if (pressingD == true && rect.x < SCREENWITDH - RECTWITDH)
 		{
 			rect.x++;
 		}
 
-		//Mirar que no surti de la pantalla
-
-		////If KeyDown
-		//projectile.x = rect.x + RECTHEIGHT / 2;
-		//projectile.y = rect.y + RECTWITDH / 2;
-		//SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-		//SDL_RenderFillRect(renderer, &projectile);
-		//projectile.x++;
-		////Borrar el missil un cop para
+		if(projectile.x <= SCREENWITDH)
+		{
+			projectile.x++;
+		}
 
 		//Rendering
 		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderFillRect(renderer, &projectile);
 
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 		SDL_RenderFillRect(renderer, &rect);
